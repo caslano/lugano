e6eccf2df06f324ffb60e5567fdb205322fc347a
caslano <35153274+caslano@users.noreply.github.com>
2018-05-25 22:18:18 +0100

    Renamed forrm1 to MainForm

---
+++ HelloWorld/HelloWorld/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello World. Modified in Master";
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void form2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var f = new Form2();
+            f.Show();
+        }
+    }
+}
